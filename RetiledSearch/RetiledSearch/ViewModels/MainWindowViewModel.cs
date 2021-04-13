c4650a7722cab71b155a8a48dee0c436823469f3
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-04-12 23:45:43 -0400

    Should work at least a little.

--- RetiledSearch/RetiledSearch/ViewModels/MainWindowViewModel.cs
+++ RetiledSearch/RetiledSearch/ViewModels/MainWindowViewModel.cs
@@ -14,19 +14,19 @@ namespace RetiledSearch.ViewModels
             // Define ProcessStartInfo.
             var SearchRunner = new ProcessStartInfo
             {
-                FileName = "https://bing.com/search?q=" & SearchTerm,
+                FileName = "https://bing.com/search?q=" + SearchTerm,
                 UseShellExecute=true
             };
 
             Process.Start(SearchRunner);
         }
 
-        private string _SearchTerm;
+        private string? _SearchTerm;
 
         public string SearchTerm
         {
             get { return _SearchTerm; }
-
+            set { _SearchTerm = value; }
         }
 
     }
