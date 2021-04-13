5ea3fe48db15d804cdfbe07602dcb17972222e6b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-04-13 19:57:24 -0400

    Copy commented code over + start writing If statement.

--- RetiledSearch/libRetiledSearch/Class1.vb
+++ RetiledSearch/libRetiledSearch/Class1.vb
@@ -1,7 +1,26 @@
+Imports System.Runtime.InteropServices
+
 Public Class Class1
 
     Public Shared Sub BeginSearch(SearchTerm As String)
+        If RuntimeInformation.IsOSPlatform(OSPlatform.Windows) Then
 
+        End If
     End Sub
+
+    ' //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    '//    {
+    '//    var SearchRunner = new ProcessStartInfo
+    '//    {
+    '//        FileName = "https://bing.com/search?q=" + SearchTerm,
+    '//        UseShellExecute = true
+    '//    };
+
+    '//    Process.Start(SearchRunner);
+    '//}
+    '//else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    '//{
+    '//    Process.Start("xdg-open", "'https://bing.com/search?q=" + SearchTerm.Replace(" ", "%20") + "'");
+    '//}
 
 End Class
