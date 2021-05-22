ccafb24944bcb1676036aa629d8dddca2d4e5f54
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-05-22 01:46:59 -0400

    Changing the debug tiles to cobalt according to W3Schools along...
    with their title.

--- RetiledStart/RetiledStart/ViewModels/MainWindowViewModel.cs
+++ RetiledStart/RetiledStart/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,11 @@ namespace RetiledStart.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "tile text";
+        public string Greeting => "cobalt-colored tile";
+        // Cobalt was #0050ef according to W3Schools, but it doesn't look quite right
+        // in Avalonia. Not sure how to make it look exactly like it did on my Lumia 822 and 830,
+        // because that's the real version of cobalt to me.
+        // #0047ab may be better, as that's what shows up when I type "cobalt" into
+        // ColorHexa.
     }
 }
