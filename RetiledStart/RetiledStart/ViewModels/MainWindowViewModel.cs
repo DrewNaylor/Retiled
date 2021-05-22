248b3f18a89d04dcf912d03a64802cab4facae47
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-05-22 01:01:50 -0400

    Change button label.

--- RetiledStart/RetiledStart/ViewModels/MainWindowViewModel.cs
+++ RetiledStart/RetiledStart/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,6 @@ namespace RetiledStart.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public string Greeting => "tile text";
     }
 }
