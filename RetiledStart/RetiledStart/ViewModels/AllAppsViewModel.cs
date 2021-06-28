8533a6927ece1827617abdf3ba0e3a4a0ff38977
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-06-28 11:25:08 -0400

    Pass exec filenames as args.

--- RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
+++ RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
@@ -10,26 +10,39 @@ namespace RetiledStart.ViewModels
     class AllAppsViewModel : ViewModelBase
     {
 
-        public void RunFirefox()
+        //public void RunFirefox()
+        //{
+        //    // Placeholder code to run Firefox for testing.
+        //    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        //    {
+        //        libRetiledStart.AppsList.RunApp(@"C:\Program Files\Mozilla Firefox\firefox.exe");
+        //    }
+        //    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        //    {
+        //        libRetiledStart.AppsList.RunApp("/usr/bin/firefox");
+        //    }
+        //}
+
+        //public void RunAngelfish()
+        //{
+        //    // Placeholder code to run Angelfish for testing.
+        //    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        //    {
+        //        libRetiledStart.AppsList.RunApp("/usr/bin/angelfish");
+        //    }
+        //}
+
+        public void RunApp(string ExecFilename)
         {
-            // Placeholder code to run Firefox for testing.
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            // Send it to the other code.
+            // The code for Firefox on Windows
+            // isn't in here anymore because I want
+            // to simplify development for now.
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                libRetiledStart.AppsList.RunApp(@"C:\Program Files\Mozilla Firefox\firefox.exe");
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                libRetiledStart.AppsList.RunApp("/usr/bin/firefox");
+                libRetiledStart.AppsList.RunApp("/usr/bin/" + ExecFilename);
             }
         }
 
-        public void RunAngelfish()
-        {
-            // Placeholder code to run Angelfish for testing.
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                libRetiledStart.AppsList.RunApp("/usr/bin/angelfish");
-            }
-        }
     }
 }
