4ae519d72d944b3f0417a96e721fb52dff153036
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-06-28 02:58:43 -0400

    Add placeholder code that runs Firefox.

--- RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
+++ RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
@@ -8,5 +8,12 @@ namespace RetiledStart.ViewModels
 {
     class AllAppsViewModel : ViewModelBase
     {
+
+        public void RunFirefox()
+        {
+            // Placeholder code to run Firefox for testing.
+            System.Diagnostics.Process.Start(@"C:\Program Files\Mozilla Firefox\firefox.exe");
+        }
+
     }
 }
