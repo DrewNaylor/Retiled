0e77bee85e268458d1de6afe7424076ce04c6eee
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-11 12:59:10 -0400

    Has a space.

--- RetiledStart/libRetiledStart/AppsList.vb
+++ RetiledStart/libRetiledStart/AppsList.vb
@@ -2,7 +2,7 @@ Public Class AppsList
 
     Public Shared Sub RunApp(ExecFilename As String)
         ' Runs what's passed to it.
-        Process.Start(ExecFilename.Replace("%u", String.Empty))
+        Process.Start(ExecFilename.Replace(" %u", String.Empty))
     End Sub
 
     Public Shared Function GetDotDesktopFiles() As ObjectModel.ObservableCollection(Of String)
