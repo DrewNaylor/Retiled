bcb1344ab6d2cd363437b83b55abd0220d872c2f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-11 09:11:03 -0400

    Writing for each code.

--- RetiledStart/libRetiledStart/AppsList.vb
+++ RetiledStart/libRetiledStart/AppsList.vb
@@ -9,6 +9,12 @@ Public Class AppsList
         ' Gets all .desktop files in /usr/share/applications
         ' on Linux or a pre-determined set on Windows.
 
+        If OperatingSystem.IsLinux = True Then
+            For Each DotDesktopFile As String In FileIO.FileSystem.GetDirectories("/usr/share/applications")
+
+            Next
+        End If
+
     End Function
 
 End Class
