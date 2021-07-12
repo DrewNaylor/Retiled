9804075cee4865c0edd88ea0d8ff8d151db660aa
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-12 12:33:40 -0400

    Delete unused code.

--- RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
+++ RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
@@ -50,56 +50,9 @@ namespace RetiledStart.ViewModels
             return desktopEntryStuff.getInfo(DotDesktopFilename, "Name");
         }
 
-
-        private string? _DotDesktopEntryName = null;
-
-        public string DotDesktopEntryName
-        {
-            get
-            {
-                if (_DotDesktopEntryName is null)
-                // Make sure the name variable isn't
-                // null, and if it is, replace it with
-                // something else.
-                {
-                    _DotDesktopEntryName = "null";
-                }
-                return _DotDesktopEntryName;
-            }
-            set { _DotDesktopEntryName = value; }
-        }
-
-
-        private string? _DotDesktopEntryCommand = null;
-
-        public string DotDesktopEntryCommand
-        {
-            get
-            {
-                if (_DotDesktopEntryCommand is null)
-                // Make sure the exec variable isn't
-                // null, and if it is, replace it with
-                // something else.
-                {
-                    _DotDesktopEntryCommand = "null";
-                }
-                return _DotDesktopEntryCommand;
-            }
-            set { _DotDesktopEntryCommand = value; }
-        }
-
         // Couldn't figure out how to do this, so I based
         // this code off this SO answer:
         // https://stackoverflow.com/a/64552332
-
-        // private ObservableCollection<string> _AllAppsListItems = new ObservableCollection<string>(new string[] { @"C:\Users\Drew\Desktop\PowerShell.desktop",
-        // @"C:\Users\Drew\Desktop\Internet Explorer.desktop", @"C:\Users\Drew\Desktop\firefox.desktop", @"C:\Users\Drew\Desktop\vim.desktop", @"C:\Users\Drew\Desktop\mousepad.desktop"});
-
-        //public ObservableCollection<string> AllAppsListItems
-        //{
-        //    get => _AllAppsListItems;
-        //    set => this.RaiseAndSetIfChanged(ref _AllAppsListItems, value);
-        //}
 
         private ObservableCollection<string> _GetDotDesktopFiles = new ObservableCollection<string> (libRetiledStart.AppsList.GetDotDesktopFiles());
 
