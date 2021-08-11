2c1f45f9cf6450158da9946a9d2f75d17142a7fa
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-08-11 09:57:01 -0400

    Now it can't find the StaticResource "Binding".

--- RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
+++ RetiledStart/RetiledStart/ViewModels/AllAppsViewModel.cs
@@ -77,7 +77,8 @@ namespace RetiledStart.ViewModels
     {
         public object Convert(IList<object> values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            string? DesktopEntryName = values[0] as string;
+            string DesktopEntryName = values[0] as string;
+            Debug.WriteLine(DesktopEntryName);
             Debug.WriteLine(desktopEntryStuff.getInfo(DesktopEntryName, "Name"));
             return desktopEntryStuff.getInfo(DesktopEntryName, "Name");
         }
