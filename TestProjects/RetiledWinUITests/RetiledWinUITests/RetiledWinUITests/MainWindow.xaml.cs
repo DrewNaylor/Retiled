25f4c4a80e30e9d9a443bd2056dab7c8868bdcdc
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-08-14 01:46:49 -0400

    Got the controls installed.

--- TestProjects/RetiledWinUITests/RetiledWinUITests/RetiledWinUITests/MainWindow.xaml.cs
+++ TestProjects/RetiledWinUITests/RetiledWinUITests/RetiledWinUITests/MainWindow.xaml.cs
@@ -28,9 +28,5 @@ namespace RetiledWinUITests
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
