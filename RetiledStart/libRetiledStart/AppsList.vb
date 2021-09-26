4f203ec6727ff95bb3ad1f356676267a0fd65deb
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-09-26 02:34:30 -0400

    Display debug output when using the context menu items...
    in the All Apps list + fix the command that's being run for each item.

--- RetiledStart/libRetiledStart/AppsList.vb
+++ RetiledStart/libRetiledStart/AppsList.vb
@@ -84,6 +84,9 @@ Public Class AppsList
 
         ' Write the tile into the startlayout file so it's pinned.
         ' TODO: Implement this.
+        Debug.WriteLine(".desktop file: " & FilenameProperty)
+        Debug.WriteLine("Width: " & TileWidth)
+        Debug.WriteLine("Height: " & TileHeight)
     End Sub
 
     Public Shared Function GetDotDesktopFiles() As ObjectModel.ObservableCollection(Of DotDesktopEntryInAllAppsList)
