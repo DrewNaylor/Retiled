5561d4a9b7558ef70fae655decd53b7db3f2eacc
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-10-01 03:41:42 -0400

    Change venv config + fix main.qml loading + remove...
    underscore from "exec" since it's deprecated.

--- RetiledSearch/PyRetiledSearch/main.py
+++ RetiledSearch/PyRetiledSearch/main.py
@@ -10,7 +10,7 @@ from PySide6.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
+    engine.load("main.qml")
     if not engine.rootObjects():
         sys.exit(-1)
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
