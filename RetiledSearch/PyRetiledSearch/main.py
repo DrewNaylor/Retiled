7cbfadae3a956eeb863ce8a503503ffb70a40a24
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-10-01 07:12:12 -0400

    Rename main window qml file + add boilerplate to main.py.
    Also delete the template main.qml.

--- RetiledSearch/PyRetiledSearch/main.py
+++ RetiledSearch/PyRetiledSearch/main.py
@@ -1,4 +1,35 @@
-# This Python file uses the following encoding: utf-8
+# PyRetiledSearch - Windows Phone 8.0-like Search app for the
+#                   Retiled project. This is a port of the original
+#                   RetiledSearch program to Python, though eventually
+#                   the name will go back to "RetiledSearch" once it's
+#                   done being ported; the "Py" in the name is temporary.
+# Copyright (C) 2021 Drew Naylor
+# (Note that the copyright years include the years left out by the hyphen.)
+# Windows Phone and all other related copyrights and trademarks are property
+# of Microsoft Corporation. All rights reserved.
+#
+# This file is a part of the Retiled project.
+# Neither Retiled nor Drew Naylor are associated with Microsoft
+# and Microsoft does not endorse Retiled.
+# Any other copyrights and trademarks belong to their
+# respective people and companies/organizations.
+#
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+
+
 import os
 from pathlib import Path
 import sys
@@ -12,8 +43,7 @@ if __name__ == "__main__":
     sys.argv += ['--style', 'Universal']
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load("Main-PyRetiledSearch.qml")
-    #engine.load("main.qml")
+    engine.load("MainWindow.qml")
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec())
