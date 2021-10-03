3e915ccd03e189489f15cf5bb5d6602fafd93d54
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-10-03 14:07:48 -0400

    Change it so it's more like a class.

--- RetiledStart/PyRetiledStart/main.py
+++ RetiledStart/PyRetiledStart/main.py
@@ -33,7 +33,7 @@
 import os
 from pathlib import Path
 import sys
-from libs.libRetiledStartPy.appslist import RunApp
+from libs.libRetiledStartPy import appslist as AppsList
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
@@ -46,7 +46,7 @@ class AllAppsListViewModel(QObject):
     def RunApp(self, ViewModelExecFilename):
         # Pass the app's command to the code to actually
         # figure out how to run it.
-        RunApp(ViewModelExecFilename)
+        AppsList.RunApp(ViewModelExecFilename)
 
 #class TilesViewModel(QObject):
     #@Slot(str)
