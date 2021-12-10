08cf691ddba39be448ccd310f5845b432de06a51
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-10 04:14:45 -0500

    Copy recursively.

--- Scripts/install-retiled.sh
+++ Scripts/install-retiled.sh
@@ -6,10 +6,12 @@ sh ./build-retiledstart.sh
 
 # Copy the files to a test directory so we can check this works:
 # https://stackoverflow.com/a/12376647
+# We have to do this recursively:
+# https://www.decodingdevops.com/copy-directory-in-linux-recursive-copy-in-linux/
 cd ..
-cp ./RetiledSearch ./test/RetiledSearch
-cp ./RetiledStyles ./test/RetiledStyles
-cp ./RetiledStart ./test/RetiledStart
+cp -rv ./RetiledSearch/* ./test/RetiledSearch
+cp -rv ./RetiledStyles/* ./test/RetiledStyles
+cp -rv ./RetiledStart/* ./test/RetiledStart
 
 # Exit with code 0.
 exit 0
