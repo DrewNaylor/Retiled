92b5fd00c286e23e2bcf9e68e482a955db3dba84
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-10 03:31:41 -0500

    Starting to write the compiling command.

--- Scripts/build-retiledsearch.sh
+++ Scripts/build-retiledsearch.sh
@@ -3,6 +3,7 @@
 # CD into the directory with the project.
 cd ../RetiledSearch/RetiledSearch
 
-# Start using py_compile to build the Python scripts:
+# Start using compileall to build the Python scripts:
 # https://docs.python.org/3/library/compileall.html
 # https://stackoverflow.com/a/32686745
+python -m compileall -l 
