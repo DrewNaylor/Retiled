14192d225f1167a54d7aceee0a8a84e7bc507bc3
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-10 03:34:34 -0500

    Compile the main.py file.

--- Scripts/build-retiledsearch.sh
+++ Scripts/build-retiledsearch.sh
@@ -6,4 +6,4 @@ cd ../RetiledSearch/RetiledSearch
 # Start using compileall to build the Python scripts:
 # https://docs.python.org/3/library/compileall.html
 # https://stackoverflow.com/a/32686745
-python -m compileall -l 
+python -m compileall -l .
