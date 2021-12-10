6868d395fbcf2b1865da8117d2eed69c0a4b0047
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-10 05:07:41 -0500

    Delete the .desktop files we install.

--- Scripts/uninstall-retiled.sh
+++ Scripts/uninstall-retiled.sh
@@ -7,5 +7,9 @@ cd ..
 # TODO: Properly uninstall and don't delete anything I didn't add.
 sudo rm -rf /opt/Retiled
 
+# Delete the .desktop files we installed.
+sudo rm -f /usr/share/applications/retiledsearch.desktop
+sudo rm -f /usr/share/applications/retiledstart.desktop
+
 # Exit with code 0.
 exit 0
