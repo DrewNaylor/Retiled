b6d15cccc0e8e4d823de14e78d695d729fb7b8fe
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-10 04:11:38 -0500

    Copy files to a test dir + don't mark the build scripts with +x...
    since we can just use "sh".

--- Scripts/install-retiled.sh
+++ Scripts/install-retiled.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# Mark the build scripts as executable.
-chmod +x ./build-retiledsearch.sh
-chmod +x ./build-retiledstart.sh
-
 # Run the build scripts.
-./build-retiledsearch.sh
-./build-retiledstart.sh
+sh ./build-retiledsearch.sh
+sh ./build-retiledstart.sh
+
+# Copy the files to a test directory so we can check this works:
+# https://stackoverflow.com/a/12376647
+cd ..
+cp ./RetiledSearch ./test/RetiledSearch
+cp ./RetiledStyles ./test/RetiledStyles
+cp ./RetiledStart ./test/RetiledStart
+
+# Exit with code 0.
+exit 0
