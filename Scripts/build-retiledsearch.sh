e67b2dc3a13f72f7f0bd6922729b9004e46fa17b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-10 03:29:30 -0500

    Add comment with links.

--- Scripts/build-retiledsearch.sh
+++ Scripts/build-retiledsearch.sh
@@ -2,3 +2,7 @@
 
 # CD into the directory with the project.
 cd ../RetiledSearch/RetiledSearch
+
+# Start using py_compile to build the Python scripts:
+# https://docs.python.org/3/library/compileall.html
+# https://stackoverflow.com/a/32686745
