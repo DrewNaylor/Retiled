12f31830997de57de64406efa42787fdeb1781ac
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-12 06:42:45 -0500

    We don't have to go up one level anymore.

--- Scripts/uninstall-retiled.sh
+++ Scripts/uninstall-retiled.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Go up one level so we can access the ./test dir.
-cd ..
-
-# Just delete the ./test dir for now.
+# Just delete the /opt/Retiled dir for now.
 # TODO: Properly uninstall and don't delete anything I didn't add.
 sudo rm -rf /opt/Retiled
 
