89b732ab3f6a3c8e40f016e2d5fee274097eb605
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-12-20 08:52:11 -0500

    Revert "Switching to using "python3" 2/4"
    This reverts commit 9b6878fdec8b510cdd3b4289bbb556075b1d0e73.

--- Scripts/build-retiledstart.sh
+++ Scripts/build-retiledstart.sh
@@ -6,13 +6,13 @@ cd ../RetiledStart/RetiledStart
 # Start using compileall to build the Python scripts:
 # https://docs.python.org/3/library/compileall.html
 # https://stackoverflow.com/a/32686745
-python3 -m compileall -l .
+python -m compileall -l .
 
 # CD into the libs folder.
 cd ./libs
 
 # Compile the stuff in here.
-python3 -m compileall .
+python -m compileall .
 
 # Go back to the original dir.
 cd ../../../Scripts
