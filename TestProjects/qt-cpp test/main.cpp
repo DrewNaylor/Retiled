aa150b32427d9bce8990751792758b204207c5e2
Drew Naylor <DrewNaylor@users.noreply.github.com>
2022-01-04 09:47:13 -0500

    Managed to get a C++ app to open the RetiledSearch window.
    There are some issues, mostly that the About page isn't completely dark mode and that you can't actually do a search.

---
+++ TestProjects/qt-cpp test/main.cpp
@@ -0,0 +1,36 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include <QLocale>
+#include <QTranslator>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    std::cout << "yo\n";
+
+    QGuiApplication app(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "test2_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            app.installTranslator(&translator);
+            break;
+        }
+    }
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"./MainWindow.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
