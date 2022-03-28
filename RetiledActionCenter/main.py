e4ac9de3fa947a48e7413b1c713b67c836d698cf
Drew Naylor <DrewNaylor@users.noreply.github.com>
2022-03-27 23:02:57 -0400

    Change boilerplate.

--- RetiledActionCenter/main.py
+++ RetiledActionCenter/main.py
@@ -1,6 +1,6 @@
-# RetiledStart - Windows Phone 8.x-like Start screen UI for the
-#                Retiled project.
-# Copyright (C) 2021 Drew Naylor
+# RetiledActionCenter - Windows Phone 8.1-like Action Center UI for the
+#                       Retiled project.
+# Copyright (C) 2022 Drew Naylor
 # (Note that the copyright years include the years left out by the hyphen.)
 # Windows Phone and all other related copyrights and trademarks are property
 # of Microsoft Corporation. All rights reserved.
