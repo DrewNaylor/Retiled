2fd46e81c1a8b19f4bcd37d6dff79550aa9dac7b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2022-07-03 18:06:40 -0400

    Copy in the getInfo() function as a base for the settings reader.

--- RetiledSettings/libs/libRetiledSettings/settingsReader.py
+++ RetiledSettings/libs/libRetiledSettings/settingsReader.py
@@ -24,6 +24,82 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+
+
+
 # configparser is used as the .config file reader.
 import configparser
 from os.path import exists
+
+def getInfo(inputFile, keyToGet, defaultValue, fileName = "", IsCustomKey = False):
+	# fileName and IsCustomKey are both optional.
+
+	# Check if the path exists first to prevent using
+	# extra memory to create the config parser if we don't have to:
+	# https://stackoverflow.com/a/8933290
+	if not exists(inputFile):
+		# Return the path if it doesn't exist, mostly
+		# in the case of getting the name.
+		# TODO: Change this to return different defaults
+		# other than the one for the name.
+		return inputFile
+	else:
+	
+	# Create a configparser to read the .desktop files.
+	# We have to change some of the options to work with
+	# only valid .desktop files by using options described
+	# in the Python docs here:
+	# https://docs.python.org/3/library/configparser.html#customizing-parser-behaviour
+	# Turn off interpolation, too, since that interferes with fields.
+	# "Strict" has to be off as some .desktop files have duplicate keys,
+	# most notably GNOME/Phosh Settings has several "X-Purism-FormFactor"
+	# keys, all with the same values.
+		dotDesktopFileReader = configparser.ConfigParser(delimiters=('='), comment_prefixes=('#'), empty_lines_in_values=False, interpolation=None, strict=False)
+	
+	# Now read the file into the dotDesktopFileReader.
+	# Basing this off this page here:
+	# https://www.tutorialspoint.com/how-to-read-a-text-file-in-python
+	# I had a TODO here about escaping backslashes, but it just does that.
+	# However, I do have to add one about removing extra quotes around
+	# the file path:
+	# TODO: Remove extra quotes around file paths if that's a problem,
+	# such as when a user pastes in a file that was copied with
+	# "Copy file as path" on Windows.
+	# Actually, configparser has a read_file function:
+	# https://docs.python.org/3/library/configparser.html#configparser.ConfigParser.read_file
+	# For some reason, I had to start specifying the encoding as UTF-8
+	# as of July 2, 2022, at least for Python on Windows. Not sure about
+	# any other platforms. I don't know why it broke suddenly.
+	# More info here:
+	# https://stackoverflow.com/a/42070962
+		dotDesktopFile = open(inputFile, "r", encoding='utf-8')
+		dotDesktopFileReader.read_file(dotDesktopFile)
+	# We can now close the file since it's in the configparser.
+		dotDesktopFile.close()
+	
+	# Now print the sections for debugging.
+	#print(dotDesktopFileReader.sections())
+	
+	# We need to specify that we'll use the Desktop Entry section.
+	# Currently I don't know how to check if it exists or not.
+	# Actually, this'll be done in the return part.
+	
+	# Python 3.10 has its own version of Select Case,
+	# but it's not stable yet, though it will be on October 4, 2021,
+	# which is the day after I'm writing this so I'll just
+	# use if statements for now. More info here:
+	# https://stackoverflow.com/a/66877137
+	# For now I'm just using an if statement as detailed here:
+	# https://stackoverflow.com/a/66886730
+		if IsCustomKey == True:
+		# Return the value of the key specified in keyToGet.
+		# This works, I just have to remember to set IsCustomKey = True
+		# for anything I haven't implemented yet.
+		# Make sure the key is in the file and return the default
+		# if it's not:
+		# https://stackoverflow.com/a/21057828
+			if dotDesktopFileReader.has_option('Desktop Entry', keyToGet):		
+				return dotDesktopFileReader.get('Desktop Entry', keyToGet)
+			else:
+				return defaultValue
+			
