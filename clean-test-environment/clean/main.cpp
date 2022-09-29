9070e99c0621515a51fcc79ae9c0e2b4592beb9f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2022-09-29 15:34:03 -0400

    Notes on working directory.

--- clean-test-environment/clean/main.cpp
+++ clean-test-environment/clean/main.cpp
@@ -2,6 +2,22 @@
 #include <QQmlApplicationEngine>
 
 
+    // NOTE: When running this program, you need to
+    // have the working directory be the source directory.
+    // So what this means is, change the working directory
+    // in Qt Creator's Projects>Build & Run>Run page to
+    // be the folder that contains "main.cpp".
+    // On Linux, if the build directory is "./build",
+    // "cd" into the folder with "main.cpp", then do
+    // "./build/appclean", or whatever the path to the
+    // executable is. Otherwise, Qt can't find the
+    // QML files correctly.
+    // There will be a script that handles the working
+    // directory properly in the future, and in fact,
+    // the plan is to install the compiled binaries
+    // in the same folders as the Python ones currently
+    // get copied to.
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
