f2492753cfb857a6e7cf89e54077f6a539239c54
Drew Naylor <DrewNaylor@users.noreply.github.com>
2023-02-27 08:22:24 -0500

    Update library to MIT version.

--- RetiledStart/RetiledStart/libs/libdotdesktop_py/desktopEntryStuff.py
+++ RetiledStart/RetiledStart/libs/libdotdesktop_py/desktopEntryStuff.py
@@ -1,24 +1,46 @@
 # libdotdesktop_py - Get info from .desktop files for the DotDesktop4Win
 # partial implementation of Freedesktop.org's Desktop Entry spec.
 # This is a limited port of libdotdesktop_standard to Python.
-# Copyright (C) 2021-2022 Drew Naylor
+# Copyright (C) 2021-2023 Drew Naylor
 # (Note that the copyright years include the years left out by the hyphen.)
 #
 # This file is a part of the DotDesktop4Win project.
 # Neither DotDesktop4Win nor Drew Naylor are associated with Freedesktop.org.
 #
 #
-#   Licensed under the Apache License, Version 2.0 (the "License");
+#   Licensed under the MIT License (the "License");
 #   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#       The MIT License (MIT)
+#       Copyright (C) 2021-2023 Drew Naylor
 #
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+#       Permission is hereby granted, free of charge,
+#       to any person obtaining a copy of this
+#       software and associated documentation files
+#       (the “Software”), to deal in the Software
+#       without restriction, including without
+#       limitation the rights to use, copy, modify,
+#       merge, publish, distribute, sublicense,
+#       and/or sell copies of the Software, and to
+#       permit persons to whom the Software is
+#       furnished to do so, subject to the following
+#       conditions:
+#
+#       The above copyright notice and this permission
+#       notice shall be included in all copies or
+#       substantial portions of the Software.
+#
+#       THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT 
+#       WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+#       INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#       OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#       PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#       THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+#       FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#       WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#       OTHERWISE, ARISING FROM, OUT OF OR IN
+#       CONNECTION WITH THE SOFTWARE OR THE USE OR
+#       OTHER DEALINGS IN THE SOFTWARE.
 
 
 
