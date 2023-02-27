c765cb5281ced73d34802c54b9b03e1085c66d6a
Drew Naylor <DrewNaylor@users.noreply.github.com>
2023-02-27 08:37:34 -0500

    Update settings reader.

--- RetiledCompositor/RetiledCompositor/libs/libRetiledSettings/settingsReader.py
+++ RetiledCompositor/RetiledCompositor/libs/libRetiledSettings/settingsReader.py
@@ -1,6 +1,6 @@
 # libRetiledSettings - Utility library for Retiled that works with settings.
 #                      This file is used for reading settings from config files.
-# Copyright (C) 2022 Drew Naylor
+# Copyright (C) 2022-2023 Drew Naylor
 # (Note that the copyright years include the years left out by the hyphen.)
 # Windows Phone and all other related copyrights and trademarks are property
 # of Microsoft Corporation. All rights reserved.
@@ -12,17 +12,39 @@
 # respective people and companies/organizations.
 #
 #
-#   Licensed under the Apache License, Version 2.0 (the "License");
+#   Licensed under the MIT License (the "License");
 #   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#       The MIT License (MIT)
+#       Copyright (C) 2022-2023 Drew Naylor
 #
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+#       Permission is hereby granted, free of charge,
+#       to any person obtaining a copy of this
+#       software and associated documentation files
+#       (the “Software”), to deal in the Software
+#       without restriction, including without
+#       limitation the rights to use, copy, modify,
+#       merge, publish, distribute, sublicense,
+#       and/or sell copies of the Software, and to
+#       permit persons to whom the Software is
+#       furnished to do so, subject to the following
+#       conditions:
+#
+#       The above copyright notice and this permission
+#       notice shall be included in all copies or
+#       substantial portions of the Software.
+#
+#       THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT 
+#       WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+#       INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#       OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#       PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#       THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+#       FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#       WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#       OTHERWISE, ARISING FROM, OUT OF OR IN
+#       CONNECTION WITH THE SOFTWARE OR THE USE OR
+#       OTHER DEALINGS IN THE SOFTWARE.
 
 
 
@@ -84,6 +106,7 @@ def getSetting(inputFile, keyToGet, defaultValue, fileName = "", IsCustomKey = T
 	# https://stackoverflow.com/a/66886730
 	# TODO: Change to using Python's Select Case statements, it's almost been
 	# a year.
+        # Update: it's been over a year.
 		if IsCustomKey == True:
 		# Return the value of the key specified in keyToGet.
 		# This works, I just have to remember to set IsCustomKey = True
