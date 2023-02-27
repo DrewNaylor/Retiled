87aba886c98d8bf177c8eb44969df11dfe1bb81c
Drew Naylor <DrewNaylor@users.noreply.github.com>
2023-02-27 09:52:11 -0500

    Boilerplate.

--- Scripts/install-retiled.sh
+++ Scripts/install-retiled.sh
@@ -1,5 +1,56 @@
 #!/bin/sh
 
+# install-retiled.sh - Install script for Retiled, 
+#                      a Windows Phone 8.x-like "desktop" environment
+#                      for (mainly) Linux phones. Will transition to being
+#                      either a Plasma Mobile fork or an alternate layout
+#                      for Plasma Mobile.
+# Copyright (C) 2021, 2023 Drew Naylor
+# (Note that the copyright years include the years left out by the hyphen.)
+# Windows Phone and all other related copyrights and trademarks are property
+# of Microsoft Corporation. All rights reserved.
+#
+# This file is a part of the Retiled project.
+# Neither Retiled nor Drew Naylor are associated with Microsoft
+# and Microsoft does not endorse Retiled.
+# Any other copyrights and trademarks belong to their
+# respective people and companies/organizations.
+#
+#
+#   Licensed under the MIT License (the "License");
+#   you may not use this file except in compliance with the License.
+#
+#       The MIT License (MIT)
+#       Copyright (C) 2021, 2023 Drew Naylor
+#
+#       Permission is hereby granted, free of charge,
+#       to any person obtaining a copy of this
+#       software and associated documentation files
+#       (the “Software”), to deal in the Software
+#       without restriction, including without
+#       limitation the rights to use, copy, modify,
+#       merge, publish, distribute, sublicense,
+#       and/or sell copies of the Software, and to
+#       permit persons to whom the Software is
+#       furnished to do so, subject to the following
+#       conditions:
+#
+#       The above copyright notice and this permission
+#       notice shall be included in all copies or
+#       substantial portions of the Software.
+#
+#       THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT 
+#       WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+#       INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#       OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#       PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#       THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+#       FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#       WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#       OTHERWISE, ARISING FROM, OUT OF OR IN
+#       CONNECTION WITH THE SOFTWARE OR THE USE OR
+#       OTHER DEALINGS IN THE SOFTWARE.
+
 # CD into ./Scripts.
 # I think it would be easier to require a manual cd into Scripts, so this is
 # uncommented until I find out an easy way to check if we're in here or not.
